da3ca4223a954ee6cc02c049ed66440ea71e3f55
MonseAC <anime.02>
2018-02-21 22:50:21 -0600

    le deje un comentario

--- app/src/main/java/itcr/mycalendar/LoginActivity.java
+++ app/src/main/java/itcr/mycalendar/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-//maee!
+//maee! que putas
 
     public void iniciarButtonOnClick(View view){
         Intent intent = new Intent(this,MainMenu.class);
